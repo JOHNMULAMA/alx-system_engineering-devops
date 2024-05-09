6b576b01780c9052c763aea26f05bf426d0f6c3e
John Mulama <johnmulama001@gmail.com>
2024-05-09 11:00:46 +0300

    testing files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Query subscribers on a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/keen-inscriber)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
